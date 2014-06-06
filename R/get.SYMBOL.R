b400d1dcabaf292a39b48c82e0e0fd054958541d
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-06 11:23:04 +0200

    Added get.SYMBOL.R

--- R/get.SYMBOL.R
+++ R/get.SYMBOL.R
@@ -1,12 +1,17 @@
 # Get Hugo gene symbols and ensembl gene identifiers
 # es in an ExpressionSet object
-get.SYMBOL <- function(es) {
-  res <- 
-    getBM(attributes = c(get.attribute(es), "hgnc_symbol", "ensembl_gene_id"),
-          filters = get.attribute(es),
-          values = featureNames(es),
-          mart = ensembl)
-  colnames(res) <- c("Probe", "GENE", "ENSG")
-  return(res)
+get.SYMBOL <- function(es, subset = featureNames(es), attributes = NULL) {
+  if (require(biomaRt)) {
+    mart <- useMart("ensembl")
+    ensembl <- useDataset("hasapiens_gene_ensembl", mart = mart)
+    res <- 
+      getBM(attributes = c(get.attribute(es), "hgnc_symbol", 
+                           "ensembl_gene_id", attributes),
+            filters = get.attribute(es),
+            values = subset,
+            mart = ensembl)
+    colnames(res) <- c("Probe", "GENE", "ENSG")
+    return(res)
+  }
 }
 
