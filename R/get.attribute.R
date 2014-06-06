4ca1b6e0bd8544681b3d366fe3f648811baf2527
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-06 11:23:32 +0200

    Added get.attributes.

--- R/get.attribute.R
+++ R/get.attribute.R
@@ -6,6 +6,6 @@ get.attribute <- function(es) {
            "hgu133plus2"  = "affy_hg_u133_plus_2",
            "hgu133a"      = "affy_hg_u133a",
            "hgu133b"      = "affy_hg_u133b",
-           "hugene10stv1" = "affy_hugene_1_0_st_v1"))
+           "hugene10stv1" = "affy_hugene_1_0_st_v1")
   return(array)
 }
