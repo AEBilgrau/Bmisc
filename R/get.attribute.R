ba0f66e7fa09e92887c6cc1e8397232cb62af729
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-08 16:04:18 +0200

    Update with new dependency

--- R/get.attribute.R
+++ R/get.attribute.R
@@ -2,7 +2,7 @@
 # es is an ExpressionSet object
 get.attribute <- function(es) {
   array <-
-    switch(biomaRt::annotation(es), 
+    switch(annotation(es), 
            "hgu133plus2"  = "affy_hg_u133_plus_2",
            "hgu133a"      = "affy_hg_u133a",
            "hgu133b"      = "affy_hg_u133b",
