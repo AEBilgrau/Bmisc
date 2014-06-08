5825237c2a6563646b5296423843c985a2627bcd
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-08 15:57:30 +0200

    Added namespace.

--- R/get.SYMBOL.R
+++ R/get.SYMBOL.R
@@ -1,15 +1,17 @@
 # Get Hugo gene symbols and ensembl gene identifiers
 # es in an ExpressionSet object
-get.SYMBOL <- function(es, subset = featureNames(es), attributes = NULL) {
+get.SYMBOL <- function(es, 
+                       subset = biomaRt::featureNames(es), 
+                       attributes = NULL) {
   if (require(biomaRt)) {
-    mart <- useMart("ensembl")
-    ensembl <- useDataset("hasapiens_gene_ensembl", mart = mart)
+    mart <- biomaRt::useMart("ensembl")
+    ensembl <- biomaRt::useDataset("hasapiens_gene_ensembl", mart = mart)
     res <- 
-      getBM(attributes = c(get.attribute(es), "hgnc_symbol", 
-                           "ensembl_gene_id", attributes),
-            filters = get.attribute(es),
-            values = subset,
-            mart = ensembl)
+      biomaRt::getBM(attributes = c(get.attribute(es), "hgnc_symbol", 
+                                    "ensembl_gene_id", attributes),
+                     filters = get.attribute(es),
+                     values = subset,
+                     mart = ensembl)
     colnames(res) <- c("Probe", "GENE", "ENSG")
     return(res)
   }
