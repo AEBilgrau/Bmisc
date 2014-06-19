d35797eae7a0713c3627b7dea5fe905f39916e54
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-19 17:11:26 +0200

    Roxygenized colSds

--- R/rowSds.R
+++ R/rowSds.R
@@ -1,6 +1,12 @@
-# x is a matrix
+#' @rdname rowSds
+#' @return \code{colSds} returns a numeric vector of length \code{m}.
+#' @examples
+#' y <- matrix(rnorm(50), 10, 5)
+#' colSds(y)
+#' @export
 rowSds <- function(x) {
   n <- ncol(x)
   means <- rowMeans(x)
   return(sqrt(rowMeans((x - means)^2)*(n/(n - 1))))
 }
+
