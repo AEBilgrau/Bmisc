4b6592dd2b7f3f66a2c4ed42b9a72a07800a0afc
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-24 13:13:52 +0200

    Added get.upper.tri.R

---
+++ R/get.upper.tri.R
@@ -0,0 +1,12 @@
+#' @rdname get.lower.tri
+#' @return \code{get.upper.tri} returns a numeric vector of the entries in the
+#'   below the diagonal of \code{x}. The length is dependent on argument 
+#'   \code{diag}.
+#' @examples
+#' print(x <- matrix(1:25, ncol = 3))
+#' get.upper.tri(x)
+#' get.upper.tri(x, diag = TRUE)
+get.upper.tri <- function (x, diag = FALSE) {
+  x[upper.tri(x, diag = diag)]
+}
+
