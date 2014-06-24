7eed7e1be914986fb6680ab4e74fbbd1f239d22c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-24 13:13:43 +0200

    Added get.upper.tri.R

---
+++ R/get.lower.tri.R
@@ -0,0 +1,19 @@
+#' Vectorize the upper and lower triangluar matrix
+#'
+#' @param x A numeric matrix.
+#' @param diag logical. Should the diagonal be included?
+#' @return \code{get.lower.tri} returns a numeric vector of the entries in the
+#'   below the diagonal of \code{x}. The length is dependent on argument 
+#'   \code{diag}.
+#' @details The returned length is dependent on \code{diag}. If 
+#'   \code{diag == TRUE}, then the length is \code{ncol(x)*(ncol(x)-1)/2}. If 
+#'   \code{FALSE}, then the length is \code{ncol(x)*(ncol(x)+1)/2}.
+#' @seealso \link{\code{lower.tri}}, \link{\code{upper.tri}}
+#' @examples
+#' print(x <- matrix(1:25, ncol = 5))
+#' get.lower.tri(x)
+#' get.lower.tri(x, diag = TRUE)
+get.lower.tri <- function (x, diag = FALSE) {
+  x[lower.tri(x, diag = diag)]
+}
+
