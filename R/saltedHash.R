ca1fb121fd16c9de536b76c57276a696feabf7f9
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-06-28 22:05:25 +0200

    Roxygen documentation.

--- R/saltedHash.R
+++ R/saltedHash.R
@@ -1,5 +1,21 @@
+#' Salted hashing function
+#'
+#' This function adds salt (stochastic or deterministic) to a character
+#' of strings prior to a hashing of the complete string.
+#' @param salt A character containing the salt
+#' @param x A character vector of the strings to be hashed.
+#' @param algo The hashing algorithm used. See \code{\link{digest}} for 
+#'   possible values.
+#' @param \dots Additional parameters passed to \code{\link{digest}}.
+#' @return A character vector
+#' @author Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
+#' @seealso \code{\link{digest}}
+#' @examples
+#' x <- c("AVerySecretString", "AnotherSecretSting")
+#' saltedHash("SomeSalt", x = x, algo = "md5")
+#' digest(paste0("SomeSalt", "AVerySecretString"), algo = "md5")
 saltedHash <- function(salt, x, algo = "sha256", ...) {
-  if (require(digest))
+  if (!require(digest))
     stop("Package digest is need to use the function.")
   z <- y <- paste0(salt, x)  # Add salt
   for (i in seq_along(y)) {  # Hashing
