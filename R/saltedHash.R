4e4e0e0510a9db990a584e8c0cb6874ac955fe3c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-07-16 00:45:01 +0200

    Fixed links. Better formatting.

--- R/saltedHash.R
+++ R/saltedHash.R
@@ -4,19 +4,22 @@
 #' of strings prior to a hashing of the complete string.
 #' @param salt A character containing the salt
 #' @param x A character vector of the strings to be hashed.
-#' @param algo The hashing algorithm used. See \code{\link{digest}} for 
+#' @param algo The hashing algorithm used. See 
+#'   \code{\link[digest;digest]{digest}} for 
 #'   possible values.
-#' @param \dots Additional parameters passed to \code{\link{digest}}.
+#' @param \dots Additional parameters passed to 
+#'   \code{\link[digest;digest]{digest}}.
 #' @return A character vector
 #' @author Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
-#' @seealso \code{\link{digest}}
+#' @seealso \code{\link[digest;digest]{digest}}
 #' @examples
 #' x <- c("AVerySecretString", "AnotherSecretSting")
 #' saltedHash("SomeSalt", x = x, algo = "md5")
 #' digest(paste0("SomeSalt", "AVerySecretString"), algo = "md5")
 saltedHash <- function(salt, x, algo = "sha256", ...) {
-  if (!require(digest))
+  if (!require(digest)) {
     stop("Package digest is need to use the function.")
+  }
   z <- y <- paste0(salt, x)  # Add salt
   for (i in seq_along(y)) {  # Hashing
     z[i] <- digest(y[i], algo = algo, ...)
