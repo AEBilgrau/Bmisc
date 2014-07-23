fbf995acabb94a63f338e4540b0c59a81f406b59
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-07-23 09:38:20 +0200

    Fixed rowSds/colSds mixup in docs.

--- R/colSds.R
+++ R/colSds.R
@@ -1,16 +1,15 @@
-#' @name rowSds
-#' @aliases rowSds
-#' @aliases colSds
+#' @name colSds
+#' @aliases rowSds colSds
 #' @title Compute row and column standard deviations
 #' @description The \code{rowSds} and \code{colSds} respectively computes the 
 #'   standard deviations of the rows and columns of the given matrix.
 #' @param x A numeric matrix of size \code{n} times \code{m}
-#' @return \code{rowSds} returns a numeric vector of length \code{n}.
+#' @return \code{colSds} returns a numeric vector of length \code{m}.
 #' @author Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
 #' @seealso \code{\link{rowMeans}}, \code{\link{colMeans}}
 #' @examples
 #' x <- matrix(rnorm(50), 10, 5)
-#' rowSds(x)
+#' colSds(x)
 #' @export
 colSds <- function(x) {
   n <- nrow(x)
