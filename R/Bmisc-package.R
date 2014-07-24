1eb9b77c559856f96dc06e5205e0b484f9135b4c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-07-24 22:28:34 +0200

    Renamed package documentation file. Added aliases.

---
+++ R/Bmisc-package.R
@@ -0,0 +1,10 @@
+#' My biostatstical miscellaneous functions
+#'
+#' My frequently used biostatisical miscellaneous R functions and utilities.
+#' Useful for general microarray analysis, gene network analysis, and other
+#' biostatistical applications.
+#' @author Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
+#' @docType package
+#' @name Bmisc-package
+#' @aliases Bmisc-package Bmisc
+NULL
--- R/Bmisc.R
+++
@@ -1,9 +0,0 @@
-#' My biostatstical miscellaneous functions 
-#'
-#' My frequently used biostatisical miscellaneous R functions and utilities. 
-#' Useful for general microarray analysis, gene network analysis, and other 
-#' biostatistical applications.
-#' @author Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
-#' @docType package
-#' @name Bmisc
-NULL
