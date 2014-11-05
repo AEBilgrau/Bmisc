3be60f7f4adbd83d9c0375a7223a73e59dee1f43
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-11-05 08:18:26 +0100

    Fixed issue with incorrectly ordered paths. Expanded documentation.

--- R/plotHierarchicalEdgeBundles.R
+++ R/plotHierarchicalEdgeBundles.R
@@ -1,8 +1,7 @@
 #' Hierarchical Edge Bundling
 #' 
-#' Visualization of networks using hierarchical edge bundles. 
-#' Currently, Bezier curves are used and not B-splines for the bundling. 
-#' Hopefully, this will be updated in future versions.
+#' Visualization of networks using hierarchical edge bundles. Plot a graph
+#' using a hierchical tree as guies for the edges.
 #' 
 #' @param phylo A `phylo` object.
 #' @param graph A \code{igraph} object.
@@ -13,12 +12,15 @@
 #' @param ... Arguments passed to \code{\link[ape]{plot.phylo}}.
 #' @param args.lines A list of arguments passed to \code{lines}.
 #' @param debug Plot some extra info.
-#' @param use.only An integer vector giving the nodes from which edges are
+#' @param v.use.only An integer vector giving the nodes from which edges are
 #'   to be drawn. E.g. \code{use.only = 1} will only plot the edges from 
 #'   vertex 1.
+#' @param e.use.only An integer vector giving the edges to be drawn. 
+#'   E.g. \code{use.only = 1} will only draw the first edge.
 #' @return Plots to a new device.
 #' @seealso See \code{\link[ape]{plot.phylo}}.
-#' @author Anders Ellern Bilgrau
+#' @author 
+#'   Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
 #' @references
 #'   Holten, Danny. "Hierarchical edge bundles: Visualization of adjacency 
 #'   relations in hierarchical data." Visualization and Computer Graphics, IEEE 
@@ -38,42 +40,43 @@
 #' 
 #' phylo <- as.phylo(hclust(as.dist(1 - adj), method = "ward.D"))
 #' 
+#' par(mfrow = c(1,3))
 #' plot(phylo, type = "fan")
 #' plotHierarchicalEdgeBundles(phylo, graph, type = "fan", beta = 0.95,
 #'                             args.lines = list(col = alp("steelblue", 90)))
-#' plotHierarchicalEdgeBundles(phylo, graph, type = "fan", beta = 0.75,
+#' plotHierarchicalEdgeBundles(phylo, graph, type = "fan", beta = 0.85,
 #'                             args.lines = list(col = alp("steelblue", 90)))
-#'                             
+#'              
+#' # Extra control of plotting and debugging               
 #' par(mfrow = c(1,2))
 #' plot(phylo, type = "fan")        
-#' plotHierarchicalEdgeBundles(phylo, graph, type = "fan", beta = 0.99,
-#'                             e.use.only = 1,
-#'                             debug = TRUE,
-#'                             args.lines = list(col = alp("steelblue", 1)))
+#' plotHierarchicalEdgeBundles(phylo, graph, type = "fan", beta = 0.95,
+#'                             v.use.only = 1, debug = FALSE,
+#'                             args.lines = list(col = alp("red", 1), lwd = 2))
 #' @export
-plotHierarchicalEdgeBundles <-
-  function(phylo, 
-           graph,
-           beta = 0.5,
-           include.mrca = FALSE,
-           simplify = FALSE,
-           ...,
-           args.lines = list(),
-           debug = FALSE,
-           v.use.only,
-           e.use.only) {
+plotHierarchicalEdgeBundles <- function(phylo, 
+                                        graph,
+                                        beta = 0.5,
+                                        include.mrca = FALSE,
+                                        simplify = FALSE,
+                                        ...,
+                                        args.lines = list(),
+                                        debug = FALSE,
+                                        v.use.only,
+                                        e.use.only) {
+  
   stopifnot(require("igraph"))
   stopifnot(require("adephylo"))
   stopifnot(require("ape"))
-    
-  plot(phylo, edge.color = "#00000000", ...)#, type = "fan")#, ...)
+  
+  plot(phylo, edge.color = ifelse(debug,"grey","#00000000"), ...)#, type = "fan")#, ...)
   # Get data
   phy.dat <- get("last_plot.phylo", envir = .PlotPhyloEnv)
   pos <- with(phy.dat, data.frame(i = seq_along(xx), x = xx, y = yy))
   # Add points
   if (debug) {
     points(pos$x, pos$y, col="black", pch = 16, cex = 0.5)
-    text(pos$x, pos$y, col="black", pos$i, cex = 2)
+    text(pos$x, pos$y, col="black", pos$i, cex = 1.5)
   }
   
   # Get edgelist (and convert to numeric if names are present)
@@ -87,7 +90,8 @@ plotHierarchicalEdgeBundles <-
   
   # Shortest paths (with start and end) or path through Most Recent Common 
   # Ancestor 
-  sp2 <- sp.tips(phylo, es[, 1], es[, 2], include.mrca = include.mrca)
+  sp2 <- #sp.tips(phylo, es[, 1], es[, 2], include.mrca = include.mrca)
+    sp.tips2(phylo, es[, 1], es[, 2], include.mrca = include.mrca)
   stopifnot(nrow(es) == length(sp2))
   sp <- lapply(seq_along(sp2), function(i) unname(c(es[i,1],sp2[[i]],es[i,2])))
   names(sp) <- names(sp2)
@@ -105,3 +109,104 @@ plotHierarchicalEdgeBundles <-
     do.call(lines, c(tmp, args.lines))
   }
 }
+
+
+# MODIFIED sp.tips from adephylo!
+sp.tips2 <- function(x, tip1, tip2, useTipNames=FALSE, 
+                     quiet=FALSE, include.mrca=TRUE) {
+  x <- as(x, "phylo4")
+  if (is.character(checkval <- checkPhylo4(x))) 
+    stop(checkval)
+  t1 <- getNode(x, tip1)
+  t2 <- getNode(x, tip2)
+  if (any(is.na(c(t1, t2)))) 
+    stop("wrong tip specified")
+  if (any(c(t1, t2) > nTips(x))) 
+    stop("specified nodes are internal nodes")
+  if (length(t1) != length(t2)) {
+    maxLength <- max(length(t1), length(t2))
+    t1 <- rep(t1, length.out = maxLength)
+    t2 <- rep(t2, length.out = maxLength)
+  }
+  toRemove <- (t1 == t2)
+  if (sum(toRemove) > 0) {
+    t1 <- t1[!toRemove]
+    t2 <- t2[!toRemove]
+    if (length(t1) == 0) 
+      stop("tip1 and tip2 are the same vectors")
+    if (!quiet) 
+      warning("tip1 and tip2 are sometimes the same; erasing these cases")
+  }
+  N <- nTips(x)
+  root <- getNode(x, N + 1)
+  E <- x@edge
+  allTips <- unique(c(t1, t2))
+  pathTwoTips <- function(path1, path2) {
+    cpath <- c(path1, rev(path2))
+    temp <- factor(cpath, levels = unique(cpath))
+    CA <- temp[table(temp) == 2][1]
+    CA <- as.integer(as.character(CA))
+    path1 <- path1[1:(which(path1 == CA))]
+    temp <- which(path2 == CA)
+    if (temp == 1) 
+      return(path1)
+    path2 <- path2[1:(temp - 1)]
+    return(c(path1, path2))
+  }
+  pathTwoTips.no.mrca <- function(path1, path2) {
+    cpath <- c(path1, rev(path2))
+    temp <- intersect(path1, path2)
+    res <- setdiff(cpath, temp)
+    return(res)
+  }
+  allPathToRoot <- lapply(allTips, function(i) 
+    .tipToRoot(x, i, root, include.root = TRUE))
+  names(allPathToRoot) <- allTips
+  allPath1 <- allPathToRoot[as.character(t1)]
+  allPath2 <- allPathToRoot[as.character(t2)]
+  if (include.mrca) {
+    res <- lapply(1:length(allPath1), function(i) 
+      pathTwoTips(allPath1[[i]], allPath2[[i]]))
+  }
+  else {
+    res <- lapply(1:length(allPath1), function(i) 
+      pathTwoTips.no.mrca(allPath1[[i]], allPath2[[i]]))
+    temp.names <- names(res)
+    temp <- sapply(res, function(vec) length(vec) > 0)
+    res[temp] <- lapply(res[temp], function(vec) getNode(x, 
+                                                         vec))
+    names(res) <- temp.names
+  }
+  if (useTipNames) {
+    names(res) <- paste(names(t1), names(t2), sep = "-")
+  }
+  else {
+    names(res) <- paste(t1, t2, sep = "-")
+  }
+  return(res)
+}  
+environment(sp.tips2) <- asNamespace("adephylo")
+
+
+# tip2tip <- function(phylo, t1, t2, include.mrca = FALSE) {
+#   x <- as(phylo, "phylo4")
+#   #     mrca <- MRCA(x, t1, t2)
+#   #     path <-  c(t1, .tipToRoot(x, t1, mrca), 
+#   #                if(include.mrca) {mrca}, 
+#   #                rev(.tipToRoot(x, t2, mrca)), t2)
+#   N <- nTips(x)
+#   root <- getNode(x, N + 1)
+#   t1 <- getNode(x, tip1)
+#   t2 <- getNode(x, tip2)
+#   allTips <- unique(c(t1, t2))    
+#   allPathToRoot <- lapply(allTips, function(i) 
+#     .tipToRoot(x, i, root, include.root =TRUE))
+#   names(allPathToRoot) <- allTips
+#   allPath1 <- allPathToRoot[as.character(t1)]
+#   allPath2 <- allPathToRoot[as.character(t2)]
+#   res <- lapply(seq_along(allPath1), function(i) 
+#     pathTwoTips(allPath1[[i]], allPath2[[i]]))
+# }
+# return(paths)
+
+
