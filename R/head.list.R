c0a7fa2fe9760f728975ca0c7416a2734f7ac88a
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-04-16 15:52:18 +0200

    Moved tail.list method to head.list.R

--- R/head.list.R
+++ R/head.list.R
@@ -33,3 +33,13 @@ head.list <- function(x, n = 6L, m = n, ...) {
   return(lapply(x[seq_len(m)],
                 function(this_x) head(this_x, n = n, ...)))
 }
+
+#' @rdname head.list
+#' @importFrom utils tail
+#' @export
+tail.list <- function(x, n = 6L, m = n, ...) {
+  stopifnot(length(n) == 1L & length(m) == 1L)
+  m <- ifelse(m < 0L, max(length(x) + m, 0L), min(m, length(x)))
+  return(lapply(x[seq.int(to = length(x), length.out = m)],
+                function(this_x) head(this_x, n = n, ...)))
+}
--- R/tail.list.R
+++
@@ -1,9 +0,0 @@
-#' @rdname head.list
-#' @importFrom utils tail
-#' @export
-tail.list <- function(x, n = 6L, m = n, ...) {
-  stopifnot(length(n) == 1L & length(m) == 1L)
-  m <- ifelse(m < 0L, max(length(x) + m, 0L), min(m, length(x)))
-  return(lapply(x[seq.int(to = length(x), length.out = m)],
-                function(this_x) head(this_x, n = n, ...)))
-}
