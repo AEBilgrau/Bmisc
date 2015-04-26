3fce97d8918ec8dac7bd7b286429e490e29a4973
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-04-26 18:08:51 +0200

    Added colourkey function.

---
+++ R/plotColourKey.R
@@ -0,0 +1,26 @@
+#' Plot a colour key
+#'
+#' Easy plotting of a colour key.
+#' 
+#' @param breaks A numeric vector of length \eqn{n} 
+#'   giving the breakpoints of the key.
+#' @param col A character vector of the colours used of length \eqn{n - 1}.
+#' @param add Should the key be added to the current plot device?
+#' @param \dots Arguments passed to \code{\link{rect}}.
+#' @author Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
+#' @examples
+#' brks <- seq(0, 1, length.out = 8)
+#' cls <- rainbow(7)
+#' plotColourKey(brks, cls)
+#' axis(1)
+#' axis(2)
+#' @export
+plotColourKey <- function(breaks, col, add = FALSE, ...) {
+  stopifnot(length(col) + 1 == length(breaks))
+  nb <- length(breaks)
+  if (!add) {
+    plot(breaks, xlim = range(breaks), ylim = c(0,1), type = "n", axes = FALSE,
+         xlab ="", ylab = "")
+  }
+  rect(breaks[-nb], 0, breaks[-1], 1, col = col, border = NA, ...)
+}
