25d4c0e529d4542ade13ff5645e1b14d8a4cc7c7
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2016-01-12 22:07:30 +0100

    Updated documentation for ones(), zeros()

--- R/ones.R
+++ R/ones.R
@@ -1,22 +1,18 @@
-#' Create an array of ones, zeros
+#' Create an array of ones or zeros
 #'
-#' Simple functions for creating an array of ones or zeros. Simply a wrapper
-#' for \code{array}.
-#' Built for convinienece and not speed.
+#' Easy interface for creating an array of supplied dimension of ones and zeros.
+#' Simply a wrapper for \code{array}. Built for convinienece and not speed.
 #' 
-#' @param \dots A list of the number of ones/zeros along each dimension.
-#'   Can also be an integer vector.
-#' @return Returns an \code{array} of the dimension specified.
+#' @param \dots A vector or list of integers giving the dimension of the array.
+#'   A dimnames argument can be supplied.
+#' @return Returns an \code{array}, \code{matrix}, or \code{vector} of the 
+#'   dimension specified by the arguments.
 #' @seealso \code{\link{array}}
 #' @examples
-#' ones(10)
-#' ones(10,5)
+#' ones(6)
+#' ones(3,5)
 #' ones(c(2,3,3))
 #' ones(3,3,3, dimnames=list(LETTERS[1:3], letters[1:3], LETTERS[4:6]))
-#' 
-#' zeros(10, 12)
-#' zeros(c(5, 3, 2))
-#' zeros(3, 4, dimnames=list(LETTERS[1:3], letters[9:12]))
 #' @export
 ones <- function(...) {
   args <- list(...)
@@ -29,6 +25,10 @@ ones <- function(...) {
 }
 
 #' @rdname ones
+#' @examples 
+#' zeros(4, 3)
+#' zeros(list(5, 3, 2))
+#' zeros(c(3, 4), dimnames=list(LETTERS[1:3], letters[9:12]))
 #' @export
 zeros <- function(...) {
   args <- list(...)
