6239b0899997529d9e57ea54e26a1f2d0244aa2d
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2016-01-14 08:20:36 +0100

    Updated flip() documentation

--- R/flip.R
+++ R/flip.R
@@ -1,15 +1,21 @@
 #' Flip vector names and values
 #' 
-#' Flips or switches names and values of vectors.
+#' Simply flips or switches values and names of named vectors. This is e.g. 
+#' useful when named character vectors are used a mapping.
 #' 
 #' @param x A named vector.
-#' @return A character vector.
+#' @return A named character vector.
 #' @author Anders Ellern Bilgrau
 #' @examples 
 #' x <- structure(1:5, names = LETTERS[1:5])
 #' x
 #' flip(x)
 #' flip(flip(x))
+#' 
+#' 
+#' y <- structure(c("a", "b", "c"), names = c("abel", "bob", "charlie"))
+#' y["bob"]
+#' flip(y)["c"]
 #' @export
 flip <- function(x) {
   stopifnot(is.vector(x))
