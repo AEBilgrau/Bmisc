2ade18d50795e98e7bda51d0fa52a9ea01bbcf41
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2016-01-15 17:29:57 +0100

    Fixed issue when no objects are in the memory.

--- R/ls.objects.R
+++ R/ls.objects.R
@@ -40,14 +40,16 @@
     sapply(names, function(x) fn(get(x, pos = pos)))
   }
   names <- ls(pos = pos, pattern = pattern)
+  if (length(names) == 0L) {
+    return(data.frame())
+  }
   obj.class <- napply(names, function(x) as.character(class(x))[1])
   obj.mode  <- napply(names, mode)
   obj.type  <- ifelse(is.na(obj.class), obj.mode, obj.class)
   obj.size  <- napply(names, object.size)
   obj.prettysize <-
     napply(names, function(x) format(object.size(x), units = "auto"))
-  obj.dim   <- t(napply(names, function(x)
-    as.numeric(dim(x))[1:2]))
+  obj.dim   <- t(napply(names, function(x) as.numeric(dim(x))[1:2]))
   vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
   obj.dim[vec, 1] <- napply(names, length)[vec]
   out <- data.frame(obj.type, obj.size, obj.prettysize, obj.dim)
