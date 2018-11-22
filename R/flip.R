c5590f4b713688eea81018629a381d576aeb02f8
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2018-11-22 19:41:51 +0100

    flip() will now attempt to coerce into a numeric by default.

--- R/flip.R
+++ R/flip.R
@@ -4,6 +4,8 @@
 #' useful when named character vectors are used a mapping.
 #' 
 #' @param x A named vector.
+#' @param convert logical. If \code{TRUE}, an attempt will be made to coerce 
+#'   the flipped values into a \code{numeric}.
 #' @return A named character vector.
 #' @author Anders Ellern Bilgrau
 #' @examples 
@@ -12,14 +14,27 @@
 #' flip(x)
 #' flip(flip(x))
 #' 
+#' y <- structure(c("a", "b", "c", "d", "e", "f", "g"), 
+#'    names = c("albert", "bob", "charlie", "dennis", "eddie", "frank", "hugo"))
+#' y["hugo"]
+#' flip(y)["a"]
 #' 
-#' y <- structure(c("a", "b", "c"), names = c("abel", "bob", "charlie"))
-#' y["bob"]
-#' flip(y)["c"]
+#' z <- sample(c(TRUE, FALSE), size = 10, replace = TRUE)
+#' names(z) <- LETTERS[seq_along(z)]
+#' z
+#' flip(z)
+#' flip(flip(z))
+#' @importFrom utils hasName
 #' @export
-flip <- function(x) {
-  stopifnot(is.vector(x))
+flip <- function(x, convert = TRUE) {
+  stopifnot(is.vector(x) & hasName(attributes(x), "names"))
   ans <- names(x)
+  if (convert) {
+    suppressWarnings(tmp_numeric <- as.numeric(ans))
+    if (!identical(tmp_numeric, rep(NA_real_,length(ans)))) {
+      ans <- tmp_numeric
+    }
+  }
   names(ans) <- x
   return(ans)
 }
